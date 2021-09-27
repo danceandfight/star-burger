9075853538b43640a4f662fab5fb8a0bef248372
danceandfight <danceandfight@gmail.com>
2021-09-27 17:18:26 +0300

    Make field address required

--- places/models.py
+++ places/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 class Place(models.Model):
-    address = models.CharField('адрес', max_length=100, blank=True, unique=True)
+    address = models.CharField('адрес', max_length=100, unique=True)
     lon = models.FloatField(verbose_name="Долгота")
     lat = models.FloatField(verbose_name="Широта")
     date = models.DateTimeField("Дата запроса к геокодеру")
-
