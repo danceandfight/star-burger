086b91768712d10efa2ebf8571d517a0ff867668
danceandfight <danceandfight@gmail.com>
2021-09-27 16:20:09 +0300

    Fix coordinate fetching

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -141,6 +141,10 @@ def fetch_coordinates(apikey, place):
     response = requests.get(base_url, params=params)
     response.raise_for_status()
     found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+    
+    if not found_places:
+        return None
+
     most_relevant = found_places[0]
     lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
     return lon, lat
