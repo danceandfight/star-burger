ec6f1095c367c85507a0228c182bb29f94b9949e
danceandfight <danceandfight@gmail.com>
2021-10-05 15:59:13 +0300

    Clean code in footcatdapp models

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -10,14 +10,18 @@ from phonenumber_field.modelfields import PhoneNumberField
 class Restaurant(models.Model):
     name = models.CharField('название', max_length=50)
     address = models.CharField('адрес', max_length=100, blank=True)
-    contact_phone = models.CharField('контактный телефон', max_length=50, blank=True)
-
-    def __str__(self):
-        return self.name
+    contact_phone = models.CharField(
+        'контактный телефон', 
+        max_length=50, 
+        blank=True
+        )
 
     class Meta:
         verbose_name = 'ресторан'
         verbose_name_plural = 'рестораны'
+
+    def __str__(self):
+        return self.name
 
 
 class ProductQuerySet(models.QuerySet):
@@ -38,32 +42,56 @@ class ProductCategory(models.Model):
 
 class Product(models.Model):
     name = models.CharField('название', max_length=50)
-    category = models.ForeignKey(ProductCategory, null=True, blank=True, on_delete=models.SET_NULL,
-                                 verbose_name='категория', related_name='products')
-    price = models.DecimalField('цена', max_digits=8, decimal_places=2, validators=[MinValueValidator(0)])
+    category = models.ForeignKey(
+        ProductCategory, 
+        null=True, 
+        blank=True, 
+        on_delete=models.SET_NULL,
+        verbose_name='категория',
+        related_name='products'
+        )
+    price = models.DecimalField(
+        'цена', 
+        max_digits=8, 
+        decimal_places=2, 
+        validators=[MinValueValidator(0)]
+        )
     image = models.ImageField('картинка')
-    special_status = models.BooleanField('спец.предложение', default=False, db_index=True)
+    special_status = models.BooleanField(
+        'спец.предложение', 
+        default=False, 
+        db_index=True
+        )
     description = models.TextField('описание', max_length=200, blank=True)
 
     objects = ProductQuerySet.as_manager()
-
-    def __str__(self):
-        return self.name
 
     class Meta:
         verbose_name = 'товар'
         verbose_name_plural = 'товары'
 
+    def __str__(self):
+        return self.name
+
 
 class RestaurantMenuItem(models.Model):
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='menu_items',
-                                   verbose_name="ресторан")
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='menu_items',
-                                verbose_name='продукт')
-    availability = models.BooleanField('в продаже', default=True, db_index=True)
-
-    def __str__(self):
-        return f"{self.restaurant.name} - {self.product.name}"
+    restaurant = models.ForeignKey(
+        Restaurant,
+        on_delete=models.CASCADE,
+        related_name='menu_items',
+        verbose_name="ресторан"
+        )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='menu_items',
+        verbose_name='продукт'
+        )
+    availability = models.BooleanField(
+        'в продаже',
+        default=True,
+        db_index=True
+        )
 
     class Meta:
         verbose_name = 'пункт меню ресторана'
@@ -71,6 +99,9 @@ class RestaurantMenuItem(models.Model):
         unique_together = [
             ['restaurant', 'product']
         ]
+
+    def __str__(self):
+        return f"{self.restaurant.name} - {self.product.name}"
 
 
 class FoodCartQuerySet(models.QuerySet):
@@ -80,19 +111,26 @@ class FoodCartQuerySet(models.QuerySet):
 
 
 class FoodCart(models.Model):
+
+    choices = (('Recieved', 'Доставлен'), ('In progress', 'В обработке'))
+    payment_methods = (('Cash', 'Наличными'), ('By card', 'Картой'))
+
     firstname = models.CharField('Имя', max_length=20)
     lastname = models.CharField('Фамилия', max_length=30)
     address = models.CharField('адрес', max_length=100, blank=True)
-    phonenumber = PhoneNumberField("Нормализованный номер владельца", max_length=20, db_index=True)
-    choices = (('Recieved', 'Доставлен'), ('In progress', 'В обработке'))
-    payment_methods = (('Cash', 'Наличными'), ('By card', 'Картой'))
+    phonenumber = PhoneNumberField(
+        'Нормализованный номер владельца', 
+        max_length=20, 
+        db_index=True
+        )
     restaurant = models.ForeignKey(
         Restaurant,
         on_delete=models.CASCADE,
         related_name='restaurant_orders',
         blank=True,
         null=True,
-        verbose_name="ресторан")
+        verbose_name="ресторан"
+        )
     status = models.CharField(
         'Статус заказа',
         max_length=30,
@@ -121,19 +159,30 @@ class FoodCart(models.Model):
         blank=True,
         null=True
         )
+    
     objects = FoodCartQuerySet.as_manager()
-
-    def __str__(self):
-        return f'{self.address} {self.firstname} {self.lastname}'
 
     class Meta:
         verbose_name = 'заказ'
         verbose_name_plural = 'заказы'
 
+    def __str__(self):
+        return f'{self.address} {self.firstname} {self.lastname}'
+
 
 class Entry(models.Model):
-    product = models.ForeignKey(Product, related_name='product_entries', null=True, on_delete=models.CASCADE)
-    order = models.ForeignKey(FoodCart, related_name='order_entries', null=True, on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product,
+        related_name='product_entries',
+        null=True,
+        on_delete=models.CASCADE
+        )
+    order = models.ForeignKey(
+        FoodCart,
+        related_name='order_entries',
+        null=True,
+        on_delete=models.CASCADE
+        )
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(
         max_digits=8,
