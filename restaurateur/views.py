d20abb1958c60b05e6efc3ab158d1144654db411
danceandfight <danceandfight@gmail.com>
2021-11-23 16:52:31 +0300

    Remove unnecessary type conversions

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -105,10 +105,10 @@ def view_restaurants(request):
 
 
 def get_menuitem_availability():
-    restaurantsmenuitems = list(RestaurantMenuItem.objects.select_related(
+    restaurantsmenuitems = RestaurantMenuItem.objects.select_related(
         'restaurant',
         'product'
-        ))
+        )    
     menuitems_availability = defaultdict(list)
     for item in restaurantsmenuitems:
         menuitems_availability[item.product].append(item.restaurant)
@@ -164,7 +164,7 @@ def view_orders(request):
         Q(address__in=restaurant_adresses)).values())
     menuitems = get_menuitem_availability()
 
-    for order in list(FoodCart.objects.get_original_price().prefetch_related('entries')):
+    for order in FoodCart.objects.get_original_price().prefetch_related('entries'):
         products = order.entries.select_related('product')
         ordered_products = [product.product for product in products]
         order_restraurants = get_suitable_restaurant(
