66cb925899c19094c086a4a6ac815520a235b900
danceandfight <danceandfight@gmail.com>
2021-11-29 16:15:33 +0300

    Add null=True to Place coordinates

--- places/models.py
+++ places/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Place(models.Model):
     address = models.CharField('адрес', max_length=100, unique=True)
-    lon = models.FloatField(verbose_name="Долгота")
-    lat = models.FloatField(verbose_name="Широта")
+    lon = models.FloatField(verbose_name="Долгота", null=True)
+    lat = models.FloatField(verbose_name="Широта", null=True)
     date = models.DateTimeField("Дата запроса к геокодеру")
