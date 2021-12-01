8751db862cc0d0fb9e5b53b131cca69eb4effb46
danceandfight <danceandfight@gmail.com>
2021-12-01 18:05:57 +0300

    Add blank=True to coordinates in Place

--- places/models.py
+++ places/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Place(models.Model):
     address = models.CharField('адрес', max_length=100, unique=True)
-    lon = models.FloatField(verbose_name="Долгота", null=True)
-    lat = models.FloatField(verbose_name="Широта", null=True)
+    lon = models.FloatField(verbose_name="Долгота", blank=True, null=True)
+    lat = models.FloatField(verbose_name="Широта", blank=True, null=True)
     date = models.DateTimeField("Дата запроса к геокодеру")
