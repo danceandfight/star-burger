521582527f44eb6992c220da1dca35c1406c55d3
danceandfight <danceandfight@gmail.com>
2022-04-26 21:52:21 +0300

    Fix host header validation

---
+++ star_burger/views.py
@@ -0,0 +1,7 @@
+from django.http import JsonResponse
+
+
+def get_my_ip(request):
+    return JsonResponse({
+        'ip': request.META.get('HTTP_X_REAL_IP') or request.META.get('REMOTE_ADDR'),
+    })
